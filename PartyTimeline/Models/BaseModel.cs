maddin25/social-chain-app2

commit e7c60d599e1d280c0853096c87eb8ff8c0e3de50

a lot of changes and too little time to comment

PartyTimeline/Models/BaseModel.cs
@@ -15,17 +15,19 @@ namespace PartyTimeline
 		private static Random idGenerator = new Random(DateTime.Now.Millisecond);
 
 		public const string UidId = "id";
+        public const string UidDateCreated = "date_created";
+        public const string UidDateModified = "date_modified";
 
-		[JsonProperty(UidId, Required = Required.Always)]
+        [JsonProperty(UidId, Required = Required.Always)]
 		[PrimaryKey, Column("_id")]
 		public long Id { get; set; }
 
 		[JsonIgnore]
-		[Column("date_created"), NotNull]
+		[Column(UidDateCreated), NotNull]
 		public DateTime DateCreated { get; set; }
 
-		[JsonProperty("updated_time", Required = Required.Always)]
-		[Column("date_modified"), NotNull]
+		[JsonProperty(UidDateModified, Required = Required.Always)]
+		[Column(UidDateModified), NotNull]
 		public DateTime DateLastModified { get; set; }
 
 		[JsonIgnore]

PartyTimeline/Models/EventImage.cs
@@ -18,8 +18,12 @@ namespace PartyTimeline
 		private string pathSmall;
 
 		public const string UidEventId = "event_id";
-		public const string UidEventMemberId = "event_member_id";
-		public const int CaptionCharacterLimit = 80;
+		public const string UidEventMemberId = "userId";
+        public const string UidPathSmall = "path_small";
+        public const string UidPathOriginal = "path_original";
+        public const string UidDateTaken = "date_taken";
+
+        public const int CaptionCharacterLimit = 80;
 
 		// TODO: how to create unique EventImage ID?
 		[JsonProperty("caption")]
@@ -36,8 +40,8 @@ namespace PartyTimeline
 			}
 		}
 
-		[JsonIgnore]
-		[Column("path_original"), NotNull, Unique]
+		[JsonProperty(UidPathOriginal)]
+		[Column(UidPathOriginal), NotNull, Unique]
 		public string PathOriginal
 		{
 			get
@@ -51,12 +55,13 @@ namespace PartyTimeline
 			set
 			{
 				pathOriginal = value;
-				OnPropertyChanged(nameof(PathOriginal));
+                OnPropertyChanged(nameof(PathSmall));
+                OnPropertyChanged(nameof(PathOriginal));
 			}
 		}
 
-		[JsonIgnore]
-		[Column("path_small"), Unique]
+        [JsonProperty(UidPathSmall)]
+		[Column(UidPathSmall), Unique]
 		public string PathSmall
 		{
 			get
@@ -71,7 +76,8 @@ namespace PartyTimeline
 			{
 				pathSmall = value;
 				OnPropertyChanged(nameof(PathSmall));
-			}
+                OnPropertyChanged(nameof(PathOriginal));
+            }
 		}
 
 		[JsonProperty(UidEventId)]
@@ -86,8 +92,8 @@ namespace PartyTimeline
 		[Column(UidEventMemberId), NotNull]
 		public long EventMemberId { get; set; }
 
-		[JsonProperty("date_taken")]
-		[Column("date_taken"), NotNull]
+		[JsonProperty(UidDateTaken)]
+		[Column(UidDateTaken), NotNull]
 		public DateTime DateTaken { get; set; }
 
 		#region UIRelated

PartyTimeline/RestClient/RestClient.cs
@@ -18,11 +18,13 @@ namespace PartyTimeline
 	{
 		protected HttpClient httpClient;
 
-		private JsonSerializerSettings serializationSettings;
+		protected JsonSerializerSettings serializationSettings;
 
 		protected string endpoint = string.Empty;
 
         protected const string sep = "/";
+        protected const string RequestGet = "GET";
+        protected const string RequestPost = "POST";
 
         protected const string serverBaseUrl = "http://lowcost-env.zk8xjtydiz.us-west-2.elasticbeanstalk.com";
 		protected const string appName = "partytimeline";
@@ -36,15 +38,22 @@ namespace PartyTimeline
 			serverUrl = UrlJoin(serverBaseUrl, appName, appApiNode, apiVersion, this.endpoint);
 			Debug.WriteLine($"Using server URL {serverUrl} for type {this.GetType().ToString()}");
 			httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(serverUrl);
+            //httpClient.Timeout = new TimeSpan(0, 0, 3); // [h, m, s]
+
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
 			serializationSettings = new JsonSerializerSettings()
 			{
-				DateTimeZoneHandling = DateTimeZoneHandling.Utc
-			};
+				DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                DateFormatString = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+            };
 		}
 
 		public async Task<List<T>> GetAsync(string custom_endpoint = null)
 		{
-			var json = await httpClient.GetStringAsync(UrlJoin(serverUrl, custom_endpoint));
+			var json = await httpClient.GetStringAsync(custom_endpoint);
 
 			var taskModels = JsonConvert.DeserializeObject<List<T>>(json, serializationSettings);
 
@@ -59,7 +68,7 @@ namespace PartyTimeline
 
 			httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-			var result = await httpClient.PostAsync(UrlJoin(serverUrl, custom_endpoint), httpContent);
+			var result = await httpClient.PostAsync(custom_endpoint, httpContent);
             LogResponse(result);
 			return result.IsSuccessStatusCode;
 		}
@@ -72,14 +81,14 @@ namespace PartyTimeline
 
 			httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-			var result = await httpClient.PutAsync(UrlJoin(serverUrl, custom_endpoint, id), httpContent);
+			var result = await httpClient.PutAsync(UrlJoin(custom_endpoint, id), httpContent);
             LogResponse(result);
             return result.IsSuccessStatusCode;
 		}
 
 		public async Task<bool> DeleteAsync(int id, T t, string custom_endpoint = null)
 		{
-			var result = await httpClient.DeleteAsync(UrlJoin(serverUrl, custom_endpoint, id));
+			var result = await httpClient.DeleteAsync(UrlJoin(custom_endpoint, id));
             LogResponse(result);
 
             return result.IsSuccessStatusCode;
@@ -95,12 +104,37 @@ namespace PartyTimeline
                     
                 return s;
             }
-            )) + sep;
+            ));
 		}
 
         protected void LogResponse(HttpResponseMessage msg)
         {string log_sep = "\n#####\n";
             Debug.WriteLine($"{log_sep}Response:\nStatusCode = {msg.StatusCode}\nRequestMessage = {msg.RequestMessage.ToString().Replace("\n", " ")}\nContent = {msg.Content}{log_sep}");
+        }
+
+        /// <summary>
+        /// Builds an html query string.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        protected string BuildHttpQuery(params string[] args)
+        {
+            int nargs = args.Length;
+            if (nargs % 2 != 0)
+            {
+                throw new ArgumentException("An even number of arguments needs to be passed");
+            }
+            if (args.Length == 0)
+            {
+                throw new ArgumentException("No query content provided");
+            }
+            
+            List<string> parts = new List<string>(nargs / 2);
+            for (int i = 0; i < nargs; i += 2)
+            {
+                parts.Add($"{args[i]}={args[i + 1]}");
+            }
+            return "?" + string.Join("&", parts);
         }
 	}
 }

PartyTimeline/RestClient/RestClientImages.cs
@@ -6,6 +6,9 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
+using Xamarin.Auth;
 using Xamarin.Forms;
 
 namespace PartyTimeline
@@ -41,12 +44,31 @@ namespace PartyTimeline
 			httpContent.Add(imageContent, "event_image_file", Path.GetFileName(path));
 			httpContent.Add(new StringContent(image.Id.ToString()), BaseModel.UidId);
 			httpContent.Add(new StringContent(image.EventId.ToString()), EventImage.UidEventId);
-			httpContent.Add(new StringContent(image.EventMemberId.ToString()), EventImage.UidEventMemberId);
+			httpContent.Add(new StringContent("10206756772397816"), EventImage.UidEventMemberId);
 			httpContent.Add(new StringContent(quality), ImageQualities.UidImageQuality);
             
 			var result = await httpClient.PostAsync(UrlJoin(serverUrl, "upload"), httpContent);
             LogResponse(result);
 			return result.IsSuccessStatusCode;
 		}
+
+        public async Task<List<EventImage>> GetEventImages(long eventId)
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(BuildHttpQuery(
+                EventImage.UidEventId, eventId.ToString(),
+                "projection", "InLineEventImage"
+                ));
+            if (!response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine($"ERROR: Failed getting the event images for event with ID {eventId}");
+                return null;
+            }
+            List<EventImage> event_images = JsonConvert.DeserializeObject<List<EventImage>>(
+                await response.Content.ReadAsStringAsync(),
+                serializationSettings
+                );
+
+            return event_images;
+        }
 	}
 }

PartyTimeline/Services/EventService.cs
@@ -101,7 +101,7 @@ namespace PartyTimeline
 					eventsRequiredUpdate.Add(fe.Id);
 				}
 			}
-			await Task.WhenAll(eventsRequiredUpdate.Select((long id) => UpdateLocalEvent(id)));
+			await Task.WhenAll(eventsRequiredUpdate.Select((long id) => UpdateEvent(id)));
 			SortEventList();
 			CurrentSyncState.EventListSyncing = false;
 			OnSyncStateChanged();
@@ -112,43 +112,44 @@ namespace PartyTimeline
 			CurrentSyncState.EventDetailsSyncing = true;
 			CurrentSyncState.EventIdSyncing = e.Id;
 			OnSyncStateChanged();
-			// TODO pull this list from the server
-			List<EventImage> serverEventImages = new List<EventImage>();
+            
+            Task<List<EventImage>> serverEventImages = Task.Run(() => clientImages.GetEventImages(e.Id));
 			List<EventImage> localEventImages = await localDb.ReadEventImages(e.Id);
 			foreach (EventImage img in localEventImages)
 			{
 				AddToEventImageList(e, img, false);
 			}
-			/**
-			foreach (EventImage serverImage in serverEventImages)
+			
+			foreach (EventImage sImg in await serverEventImages)
 			{
-				if (localEventImages.Contains(serverImage))
+				if (localEventImages.Contains(sImg))
 				{
-					EventImage lImg = localEventImages[localEventImages.IndexOf(serverImage)];
-					if (lImg.DateLastModified < serverImage.DateLastModified) // the local event image is outdated
+					EventImage lImg = localEventImages[localEventImages.IndexOf(sImg)];
+					if (lImg.DateLastModified < sImg.DateLastModified) // the local event image is outdated
 					{
-						// TODO: mark this image to be updated
+                        lImg.Update(sImg);
 					}
 				}
 				else
 				{
-					// TODO: mark this image to be updated
+                    // TODO: check, if the user who took this image is already in the DB
+                    AddToEventImageList(e, sImg, true);
 				}
 			}
-			*/
-			// TODO: update all marked images
+			
 			SortEventImageList(e);
 			CurrentSyncState.EventDetailsSyncing = false;
 			OnSyncStateChanged();
 		}
 
-		public async Task UpdateLocalEvent(long eventId)
+		public async Task UpdateEvent(long eventId)
 		{
 			Event fe = await clientFb.GetEventDetails(eventId);
 			Task persistTask = PersistElement(fe);
 			Task<bool> postTask = clientEvents.PostAsync(fe);
 			AddToEventList(fe, true);
 			await Task.WhenAll(persistTask, postTask);
+            Debug.WriteLine($"Finished updating event with ID {eventId} (local & server)");
 		}
 
 		public async Task PersistElement(BaseModel element)

commit 5e7e26aca2fafaef4c5c3c2300959015427e12f3

fixed bug introduced in e7c60d599e1d280c0853096c87eb8ff8c0e3de50

PartyTimeline/Models/BaseModel.cs
@@ -26,7 +26,7 @@ namespace PartyTimeline
 		[Column(UidDateCreated), NotNull]
 		public DateTime DateCreated { get; set; }
 
-		[JsonProperty(UidDateModified, Required = Required.Always)]
+		[JsonProperty(UidDateModified)]
 		[Column(UidDateModified), NotNull]
 		public DateTime DateLastModified { get; set; }
 

PartyTimeline/Models/Event.cs
@@ -27,6 +27,10 @@ namespace PartyTimeline
 		[Column("end_date")]
 		public DateTime EndDateTime { get; set; }
 
+        [JsonProperty("updated_time")]
+        [Ignore]
+        private DateTime UpdatedTime { set { DateLastModified = value; } }
+
 		[JsonProperty("cover")]
 		[Ignore]
 		public CoverImage Cover { get; set; }

PartyTimeline/RestClient/FacebookClient.cs
@@ -68,7 +68,7 @@ namespace PartyTimeline
 
 		public async Task<List<Event>> GetEventHeaders()
 		{
-            Debug.WriteLine($"Getting Facebook event headers for {SessionInformationProvider.INSTANCE.CurrentUserEventMember.Name}");
+            Debug.WriteLine($"Facebook:GetEventHeaders for {SessionInformationProvider.INSTANCE.CurrentUserEventMember.Name}");
             List<Event> events = new List<Event>();
 
 			var initialRequest = new OAuth2Request(
@@ -90,7 +90,8 @@ namespace PartyTimeline
 			{
 				return;
 			}
-			List<Event> eventsInResponse = JObject.Parse(response).SelectToken("data").ToObject<List<Event>>();
+            var data = JObject.Parse(response).SelectToken("data");
+            List<Event> eventsInResponse = data.ToObject<List<Event>>();
 			DateTime toleranzeInPast = DateTime.Now.Subtract(EventService.LimitEventsInPast);
 			foreach (Event eventReference in eventsInResponse)
 			{
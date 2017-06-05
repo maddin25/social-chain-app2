5412f8b3cde1e9e122a6095372ae5d663e2f91ff
Martin Patz <mailto@martin-patz.de>
2017-06-05 18:41:55 +0200

    specified new endpoint for getting the event images

--- PartyTimeline/RestClient/RestClientImages.cs
+++ PartyTimeline/RestClient/RestClientImages.cs
@@ -14,55 +14,59 @@ using Xamarin.Forms;
 
 namespace PartyTimeline
 {
-	public class RestClientImages : RestClient<EventImage>
-	{
+    public class RestClientImages : RestClient<EventImage>
+    {
         const string EndpointEventImage = "event_image";
         const string EndpointEventImages = "event_images";
         const string EndpointUpload = "upload";
+        const string EndpointSearch = "search";
+        const string EndpointFindByEventId = "findByEventId";
 
         public RestClientImages() : base(EndpointEventImage)
-		{
+        {
 
-		}
+        }
 
-		public async Task<bool> UploadImage(EventImage image, string quality)
-		{
+        public async Task<bool> UploadImage(EventImage image, string quality)
+        {
             Debug.WriteLine($"{nameof(UploadImage)}: uploading {nameof(EventImage)} with '{EventImage.UidId}': {image.Id}");
 
             string path;
-			switch (quality)
-			{
-				case ImageQualities.SMALL:
-					path = image.PathSmall;
-					break;
-				case ImageQualities.ORIGINAL:
-					path = image.PathOriginal;
-					break;
-				default:
-					Debug.WriteLine($"Unknown quality identifier: {quality}");
-					return false;
-			}
-			var httpContent = new MultipartFormDataContent();
-			HttpContent imageContent = new StreamContent( new MemoryStream(
-				DependencyService.Get<SystemInterface>().ReadFile(path)
-			));
-			httpContent.Add(imageContent, "event_image_file", Path.GetFileName(path));
-			httpContent.Add(new StringContent(image.Id.ToString()), BaseModel.UidId);
-			httpContent.Add(new StringContent(image.EventId.ToString()), EventImage.UidEventId);
-			httpContent.Add(new StringContent("10206756772397816"), EventImage.UidEventMemberId);
-			httpContent.Add(new StringContent(quality), ImageQualities.UidImageQuality);
-            
-			var response = await httpClient.PostAsync(UrlJoin(EndpointEventImage, EndpointUpload), httpContent);
+            switch (quality)
+            {
+                case ImageQualities.SMALL:
+                    path = image.PathSmall;
+                    break;
+                case ImageQualities.ORIGINAL:
+                    path = image.PathOriginal;
+                    break;
+                default:
+                    Debug.WriteLine($"Unknown quality identifier: {quality}");
+                    return false;
+            }
+            var httpContent = new MultipartFormDataContent();
+            HttpContent imageContent = new StreamContent(new MemoryStream(
+                DependencyService.Get<SystemInterface>().ReadFile(path)
+            ));
+            httpContent.Add(imageContent, "event_image_file", Path.GetFileName(path));
+            httpContent.Add(new StringContent(image.Id.ToString()), BaseModel.UidId);
+            httpContent.Add(new StringContent(image.EventId.ToString()), EventImage.UidEventId);
+            httpContent.Add(new StringContent("10206756772397816"), EventImage.UidEventMemberId);
+            httpContent.Add(new StringContent(quality), ImageQualities.UidImageQuality);
+
+            var response = await httpClient.PostAsync(UrlJoin(EndpointEventImage, EndpointUpload), httpContent);
             LogResponse(response);
-			return response.IsSuccessStatusCode;
-		}
+            return response.IsSuccessStatusCode;
+        }
 
         public async Task<List<EventImage>> GetEventImages(long eventId)
         {
-            HttpResponseMessage response = await httpClient.GetAsync(EndpointEventImages + BuildHttpQuery(
-                EventImage.UidEventId, eventId.ToString(),
-                "projection", "InLineEventImage"
-                ));
+            HttpResponseMessage response = await httpClient.GetAsync(
+                UrlJoin(EndpointEventImages, EndpointSearch, EndpointFindByEventId) + BuildHttpQuery(
+                    EventImage.UidEventId, eventId.ToString(),
+                    "projection", "InLineEventImage"
+                )
+            );
             LogResponse(response);
             if (!response.IsSuccessStatusCode)
             {
@@ -74,5 +78,5 @@ namespace PartyTimeline
             List<EventImage> event_images = JsonConvert.DeserializeObject<List<EventImage>>(images_list.ToString(), serializationSettings);
             return event_images;
         }
-	}
+    }
 }
